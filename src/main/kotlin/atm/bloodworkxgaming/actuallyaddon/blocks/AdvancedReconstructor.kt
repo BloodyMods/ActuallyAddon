ad95b6954de85e30e9b290ef9f7618d7d7fad5d1
BloodWorkXGaming <bloodworkxgaming@gmail.com>
2018-09-17 18:18:05 +0200

    glowing warning sign

--- src/main/kotlin/atm/bloodworkxgaming/actuallyaddon/blocks/AdvancedReconstructor.kt
+++ src/main/kotlin/atm/bloodworkxgaming/actuallyaddon/blocks/AdvancedReconstructor.kt
@@ -1,12 +1,13 @@
 package atm.bloodworkxgaming.actuallyaddon.blocks
 
 import atm.bloodworkxgaming.actuallyaddon.extensions.registerMe
+import atm.bloodworkxgaming.bloodyLib.registry.IHasModel
 import net.minecraft.block.Block
 import net.minecraft.block.ITileEntityProvider
 import net.minecraft.block.material.Material
 import net.minecraft.world.World
 
-class AdvancedReconstructor : Block(Material.IRON), ITileEntityProvider {
+class AdvancedReconstructor : Block(Material.IRON), ITileEntityProvider, IHasModel {
     init {
         registerMe("advanced_reconstructor")
     }
